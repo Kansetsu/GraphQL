e8af70d7151961f6b8fc923fcf0e55a11e6cab52
Vinicius-AndradeM <vinicius802010@gmail.com>
2022-06-06 20:16:16 -0300

    Started a few exercises about Schemas & Queries.

---
+++ schema-query/ index.js
@@ -0,0 +1,53 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+    scalar Date
+
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
+    # Pontos de entrada da sua API!
+    type Query {
+        ola: String!
+        horaAtual: Date!
+        usuarioLogado: Usuario
+    }
+`;
+
+const resolvers = {
+    Query: {
+        ola() {
+            return "Vai tomar no cu!";
+        },
+
+        horaAtual() {
+            return new Date();
+        },
+
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: "Ana da Web",
+                email: "anadaweb@email.com",
+                idade: 23,
+                salario: 1234.56,
+                vip: true,
+            };
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`);
+});
